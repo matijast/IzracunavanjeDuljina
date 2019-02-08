cc686d4c640f53a8e7c1860c20f49058068fe5e8
Matija Stefanic <matijastefanic@hotmail.com>
2019-02-08 17:45:03 +0100

    cetvrti

--- IzracunavanjeDuljina_Sish/IzracunavanjeDuljina_Sish/Program.cs
+++ IzracunavanjeDuljina_Sish/IzracunavanjeDuljina_Sish/Program.cs
@@ -14,22 +14,52 @@ namespace IzracunavanjeDuljina_Sish
             int jedinice;
             Jedinice jed;
             List<double> myarray = new List<double>();
+          //  IFormatProvider provider;
 
-            Console.WriteLine("U kojim jedinicama želiš izračun:");
+            Console.WriteLine("U kojim mjernim jedinicama želiš izračun:");
             Console.WriteLine("1 za milimetre\n2 za metre\n3 za kilometre\n4 za inče\n5 za stope\n6 za milje");
             jedinice = Console.Read();
             Console.WriteLine(jedinice);
-            while(true)
+           // provider = System.Globalization.CultureInfo.CreateSpecificCulture("en-GB");
+
+            while (true)
             {
-                Console.WriteLine("Unesi jedinicu i mjernu jedinicu");
+                Console.WriteLine("Unesi jedinicu(decimalni zarez) i mjernu jedinicu, 0 za prekid");
                 string str = Console.ReadLine();
-                jed.Parse(str);
-                
+                if (str == "0")
+                    break;
+                switch (jedinice) {
+                    case '1':
+                        jed = Jedinice.Parse(str);
+                        myarray.Add(jed.Milimetri);
+                        break;
+                    case '2':
+                        jed = Jedinice.Parse(str);
+                        myarray.Add(jed.Metri);
+                        break;
+                    case '3':
+                        jed = Jedinice.Parse(str);
+                        myarray.Add(jed.Kilometri);
+                        break;
+                    case '4':
+                        jed = Jedinice.Parse(str);
+                        myarray.Add(jed.Inči);
+                        break;
+                    case '5':
+                        jed = Jedinice.Parse(str);
+                        myarray.Add(jed.Stope);
+                        break;
+                    case '6':
+                        jed = Jedinice.Parse(str);
+                        myarray.Add(jed.Milje);
+                        break;
+                }
             }
-             
+            double rezultat=0;
+            foreach (double var in myarray)
+                rezultat+= var;
 
-
-            
+            Console.WriteLine("Rezultat je " + rezultat);
             Console.ReadKey(true);
         }
     }
